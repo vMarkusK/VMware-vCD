84ba1f13ff75ef1bb08d1bea317da9c4591c674c
mycloudrevolution <mycloudrevolution@users.noreply.github.com>
2017-03-28 16:11:15 +0200

    Base EdgeReport

---
+++ InProgress/vCD-EdgeGateway-IP-Report.ps1
@@ -0,0 +1,86 @@
+## Get-IPrange Function from https://gallery.technet.microsoft.com/scriptcenter/List-the-IP-addresses-in-a-60c5bb6b
+function Get-IPrange
+{
+<# 
+  .SYNOPSIS  
+    Get the IP addresses in a range 
+  .EXAMPLE 
+   Get-IPrange -start 192.168.8.2 -end 192.168.8.20 
+  .EXAMPLE 
+   Get-IPrange -ip 192.168.8.2 -mask 255.255.255.0 
+  .EXAMPLE 
+   Get-IPrange -ip 192.168.8.3 -cidr 24 
+#> 
+ 
+param 
+( 
+  [string]$start, 
+  [string]$end, 
+  [string]$ip, 
+  [string]$mask, 
+  [int]$cidr 
+) 
+ 
+function IP-toINT64 () { 
+  param ($ip) 
+ 
+  $octets = $ip.split(".") 
+  return [int64]([int64]$octets[0]*16777216 +[int64]$octets[1]*65536 +[int64]$octets[2]*256 +[int64]$octets[3]) 
+} 
+ 
+function INT64-toIP() { 
+  param ([int64]$int) 
+
+  return (([math]::truncate($int/16777216)).tostring()+"."+([math]::truncate(($int%16777216)/65536)).tostring()+"."+([math]::truncate(($int%65536)/256)).tostring()+"."+([math]::truncate($int%256)).tostring() )
+} 
+ 
+if ($ip) {$ipaddr = [Net.IPAddress]::Parse($ip)} 
+if ($cidr) {$maskaddr = [Net.IPAddress]::Parse((INT64-toIP -int ([convert]::ToInt64(("1"*$cidr+"0"*(32-$cidr)),2)))) } 
+if ($mask) {$maskaddr = [Net.IPAddress]::Parse($mask)} 
+if ($ip) {$networkaddr = new-object net.ipaddress ($maskaddr.address -band $ipaddr.address)} 
+if ($ip) {$broadcastaddr = new-object net.ipaddress (([system.net.ipaddress]::parse("255.255.255.255").address -bxor $maskaddr.address -bor $networkaddr.address))} 
+ 
+if ($ip) { 
+  $startaddr = IP-toINT64 -ip $networkaddr.ipaddresstostring 
+  $endaddr = IP-toINT64 -ip $broadcastaddr.ipaddresstostring 
+} else { 
+  $startaddr = IP-toINT64 -ip $start 
+  $endaddr = IP-toINT64 -ip $end 
+} 
+ 
+ 
+for ($i = $startaddr; $i -le $endaddr; $i++) 
+{ 
+  INT64-toIP -int $i 
+}
+
+}
+
+$EdgeName = '*'
+$EdgeView = Search-Cloud -QueryType EdgeGateway -Name $EdgeName | Get-CIView
+
+$EdgeReport = @()
+foreach ($Edge in $EdgeView) {
+
+    [Array]$GatewayInterfaces = $Edge.Configuration.GatewayInterfaces
+    foreach($GatewayInterface in $GatewayInterfaces.GatewayInterface){ 
+                $obj = "" | Select-Object EdgeName, NetworkName, IP, IPRanges              
+                $obj.EdgeName = $Edge.Name
+                $obj.NetworkName = $GatewayInterface.Name
+                $obj.IP = $GatewayInterface.SubnetParticipation.IpAddress
+                [Array] $RangeReport = @()
+                foreach ($IPRange in $GatewayInterface.SubnetParticipation.IpRanges.IpRange) {
+                    [Array] $Range = Get-IPrange -start $IPRange.StartAddress -end $IPRange.EndAddress
+                    foreach($IP in $Range){  
+                        $RangeReport += $IP
+                        }
+                    }
+                    
+                $obj.IPRanges = $RangeReport -join "," 
+                $EdgeReport += $obj     
+                }
+                
+               
+    }
+#$EdgeReport | Export-Csv C:\test.csv -Delimiter ";"
+$EdgeReport | Sort EdgeName, NetworkName | Format-Table -AutoSize
